ea5d772ec6b91cc66e52725544c3882ad6fe2515
Larisa Ioana <larisaoltean27@outlook.com>
2024-09-05 21:05:20 +0300

    frontend origin changed

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://user-auth-frontend-ruby.vercel.app",
     credentials: true
 }));
 
